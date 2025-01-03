5a3736f8a20bbb4c1d81783d28001cebae5fc27b
sanskar <sanskaryeeha@gmail.com>
2025-01-03 17:57:30 +0530

    added events

--- src/component/Button.jsx
+++ src/component/Button.jsx
@@ -16,18 +16,36 @@ const Button = () => {
     // }
 
     //now we use conditions
-    let count = 0;
-    const handleClick = (name) => {
-        if(count < 3){
-            count++;
-            console.log(`${name} , you clicked me ${count} times`);
-        }
-        else{
-            console.log(`${name} Stop Clicking Me!!`)
-        }
-    }
+    // let count = 0;
+    // const handleClick = (name) => {
+    //     if(count < 3){
+    //         count++;
+    //         console.log(`${name} , you clicked me ${count} times`);
+    //     }
+    //     else{
+    //         console.log(`${name} Stop Clicking Me!!`)
+    //     }
+    // }
 
-    return(<button className="bg-blue-400 hover:bg-blue-200 p-2 font-bold font-mono rounded-full m-2 " onClick={() => {handleClick('Sanskar')}}>Click Me</button>)
+    //with clic events we already are given with event argumenmt ( event is an objec that tells te event occured)
+    //often shortened with e
+    //print e gives a lot of objects and one is target.textContent
+    //We can also use onDoubleClick Handler
+    let count =0;
+    const handleClick = (e) =>{
+        
+        // console.log(e)
+        if(count < 3){
+            e.target.textContent = "Click again"
+            count++;
+            console.log(count)
+            }
+        else{
+                    e.target.textContent = "Successfully clicked"
+            }
+        
+    }
+    return(<button className="bg-blue-400 hover:bg-blue-200 p-2 font-bold font-mono rounded-full m-2 " onClick={(e) => {handleClick(e)}}>Click Me</button>)
 
 
 
